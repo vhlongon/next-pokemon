a09bfb7f08fbf2ef3a961f554842eb75c90f505e
victor.longon <vhlongon@gmail.com>
2021-11-22 21:10:49 +0100

    add animate prop for LoadingBall

--- components/LoadingBall.tsx
+++ components/LoadingBall.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './loadingBall.module.css';
 
-const Pokeball = () => {
+interface Props {
+  animate?: boolean;
+}
+const Pokeball = ({ animate = true }: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={135}
       height={135}
-      className={styles.spinning}
+      className={animate ? styles.spinning : ''}
     >
       <style>
         {
