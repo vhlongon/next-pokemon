66be04602f828be9a22addf12fdadd738d7356b0
victor.longon <vhlongon@gmail.com>
2021-11-24 21:39:23 +0100

    create a habbits page and have start page to search

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,27 +4,30 @@ import React from 'react';
 
 const NavBar = () => {
   const router = useRouter();
-  const isHabitatPath = router.pathname.includes('/habitat');
+  const isHabitatsPath = router.pathname.includes('/habitats');
   const isPokemonPath = router.pathname.includes('/pokemon');
   const isStart = router.pathname === '/';
 
-  if (isStart) {
-    return null;
-  }
-
   return (
-    <nav className="flex flex-row-reverse justify-between py-2 px-4 items-center w-full bg-gray-100 border-b-2 border-indigo-100">
-      {isHabitatPath && (
+    <nav className="flex flex-row-reverse flex-end py-2 px-4 items-center w-full bg-gray-100 border-b-2 border-indigo-100">
+      {!isHabitatsPath && (
+        <Link href="/habitats" passHref>
+          <span className="cursor-pointer inline-flex ml-4 items-center justify-center bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+            Habitats
+          </span>
+        </Link>
+      )}
+      {!isStart && (
         <Link href="/" passHref>
-          <span className="cursor-pointer inline-flex items-center justify-center bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
-            Back to habitats
+          <span className="cursor-pointer inline-flex ml-4 items-center justify-center bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+            Back to start
           </span>
         </Link>
       )}
 
       {isPokemonPath && (
         <button
-          className="cursor-pointer inline-flex items-center justify-center bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+          className="cursor-pointer inline-flex ml-4 items-center justify-center bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => router.back()}
         >
           Go back
